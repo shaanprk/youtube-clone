d2c94aec348e9fb9adca89f0fc849383f45e99f7
Sean Park <imbinpark@gmail.com>
2024-12-05 18:42:52 -0800

    Created docker file

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Missing input or output file path");
+        return;
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // 360p resolution
+        .on("end", () => {
+            res.status(200).send("Video processed successfully.");
+        })
+        .on("error", (err) => {
+            console.error(`An error has occurred: ${err.message}`);
+            res.status(500).send("Internal Server Error: ${err.message}");
+        })
+        .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Video processing service listening at http://localhost:${port}`);
 });
