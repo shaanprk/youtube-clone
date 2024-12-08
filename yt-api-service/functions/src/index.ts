b93277cdd503e91cd620835fbfa57831860f19e1
Sean Park <imbinpark@gmail.com>
2024-12-07 18:03:28 -0800

    Getting weird firebase error with google-cloud/storage

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -2,10 +2,17 @@ import * as functions from "firebase-functions/v1";
 import { initializeApp } from "firebase-admin/app";
 import { Firestore } from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import { onCall } from "firebase-functions/v2/https";
+import { Storage } from "@google-cloud/storage";
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+console.log(storage);
+
+const rawVideoBucketName = "binni-yt-raw-videos";
+console.log(rawVideoBucketName);
 
 export const createUser = functions.auth.user().onCreate((user) => {
     const userInfo = {
@@ -17,4 +24,28 @@ export const createUser = functions.auth.user().onCreate((user) => {
     firestore.collection("users").doc(user.uid).set(userInfo);
     logger.info(`User Created: ${JSON.stringify(userInfo)}`);
     return;
+});
+
+export const generateUploadUrl = onCall({ maxInstances: 1 }, async (request) => {
+    if (!request.auth) {
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The function must be called while authenticated."
+        );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    // const bucket = storage.bucket(rawVideoBucketName);
+
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+    console.log(fileName);
+
+    // const [url] = await bucket.file(fileName).getSignedUrl({
+    //     version: "v4",
+    //     action: "write",
+    //     expires: Date.now() + 15 * 60 * 1000,
+    // });
+
+    // return { url, fileName };
 });
