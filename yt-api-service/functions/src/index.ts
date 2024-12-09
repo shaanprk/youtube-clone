a85b62bbdaf09d214cf01952f380de00a0fde3da
Sean Park <imbinpark@gmail.com>
2024-12-08 22:16:16 -0800

    Refactor: Update yt-api-service and yt-web-client

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -2,17 +2,15 @@ import * as functions from "firebase-functions/v1";
 import { initializeApp } from "firebase-admin/app";
 import { Firestore } from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+// import { Storage } from "@google-cloud/storage";
 import { onCall } from "firebase-functions/v2/https";
-import { Storage } from "@google-cloud/storage";
 
 initializeApp();
 
 const firestore = new Firestore();
-const storage = new Storage();
-console.log(storage);
+// const storage = new Storage();
 
-const rawVideoBucketName = "binni-yt-raw-videos";
-console.log(rawVideoBucketName);
+// const rawVideoBucketName = "binni-yt-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
     const userInfo = {
@@ -27,6 +25,7 @@ export const createUser = functions.auth.user().onCreate((user) => {
 });
 
 export const generateUploadUrl = onCall({ maxInstances: 1 }, async (request) => {
+    // Check if the user is authentication
     if (!request.auth) {
         throw new functions.https.HttpsError(
             "failed-precondition",
@@ -34,17 +33,18 @@ export const generateUploadUrl = onCall({ maxInstances: 1 }, async (request) => 
         );
     }
 
-    const auth = request.auth;
-    const data = request.data;
+    // const auth = request.auth;
+    // const data = request.data;
     // const bucket = storage.bucket(rawVideoBucketName);
 
-    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
-    console.log(fileName);
+    // Generate a unique filename for upload
+    // const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
 
+    // Get a v4 signed URL for uploading file
     // const [url] = await bucket.file(fileName).getSignedUrl({
     //     version: "v4",
     //     action: "write",
-    //     expires: Date.now() + 15 * 60 * 1000,
+    //     expires: Date.now() + 15 * 60 * 1000, // 15 minutes
     // });
 
     // return { url, fileName };
